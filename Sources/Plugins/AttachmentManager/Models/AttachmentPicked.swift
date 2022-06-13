52e895762f96f394cde4c1f1b3dd7cc4387ed7a7
brunoantoninho <bruno.antoninho@deemaze.com>
2022-06-13 10:30:28 +0100

    Implementation of custom attachment cell

---
+++ Sources/Plugins/AttachmentManager/Models/AttachmentPicked.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bruno Antoninho on 07/06/2022.
+//
+
+import Foundation
+
+open class AttachmentPicked {
+    
+    open var fileType: String!
+    open var fileName: String!
+    open var fileSize: Int!
+    open var mimeType: String!
+    open var blob: String!
+    open var uncompressedFile: Data!
+
+    public init() {}
+}
